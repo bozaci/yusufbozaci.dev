4d9ce7702d6aad6f2103eeb92248b3ff6a7d337f
bozaci <yusufonline@yandex.com>
2024-12-29 04:40:02 +0300

    chore(build): upgrade Next.js to the latest version

--- src/components/pages/content-detail/states/loading/content-detail-loading.tsx
+++ src/components/pages/content-detail/states/loading/content-detail-loading.tsx
@@ -8,7 +8,7 @@ const ContentDetailLoading = () => {
         <Skeleton width="75%" height={28} style={{ marginBottom: 5 }} />
         <Skeleton count={3} style={{ marginTop: 5 }} />
         <Skeleton height={120} style={{ marginTop: 25 }} />
-        
+
         <div style={{ marginTop: 60 }}>
           <Skeleton count={20} style={{ marginBottom: 10 }} />
         </div>
